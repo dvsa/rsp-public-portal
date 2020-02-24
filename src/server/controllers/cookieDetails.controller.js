02c33e30081d3b00327e0b76ea066ce97f84b5a9
mgoldson <marlon.goldson@bjss.com>
2020-02-24 11:10:16 +0000

    rsp-1888 -> 1890: Implicit return for route

--- src/server/controllers/cookieDetails.controller.js
+++ src/server/controllers/cookieDetails.controller.js
@@ -1,13 +1,2 @@
-import config from '../config';
-
 // Index Route
-export const index = (req, res) => {
-  const language = req.i18n_lang;
-  if (config.isDevelopment()) {
-    return res.render('cookies/details');
-  }
-  if (language === 'en') {
-    return res.render('cookies/details');
-  }
-  return res.render('cookies/details');
-};
+export const index = (req, res) => res.render('cookies/details');

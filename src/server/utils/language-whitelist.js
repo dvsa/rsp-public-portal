542938025593ab982919714a57e21b54b56354a7
mgoldson <marlon.goldson@bjss.com>
2020-10-28 15:30:56 +0000

    rsp-1955: Add new languages to whitelist

--- src/server/utils/language-whitelist.js
+++ src/server/utils/language-whitelist.js
@@ -1,1 +1,25 @@
-export const whitelist = ['en', 'fr', 'de', 'cy', 'es', 'pl'];
+export const whitelist = [
+  'en',
+  'fr',
+  'de',
+  'cy',
+  'es',
+  'pl',
+  'bg',
+  'hr',
+  'cz',
+  'nl',
+  'ee',
+  'gr',
+  'hu',
+  'it',
+  'lv',
+  'lt',
+  'pt',
+  'ro',
+  'ru',
+  'rs',
+  'sk',
+  'sl',
+  'tr'
+];

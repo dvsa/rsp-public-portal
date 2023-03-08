fa51b101c2d9154e75dcda945854c41dce0bcf3f
Ibrahim Jassat <31094657+ibrahimjassat@users.noreply.github.com>
2023-03-08 16:00:08 +0000

    feat: remove the cypress test  (#176)
    feat: remove the cypress test from the frontend repo as they are now covered in the full E2E test pack

--- cypress/e2e/common/choose-valid-language.js
+++
@@ -1,6 +0,0 @@
-import { When } from "cypress-cucumber-preprocessor/steps";
-
-When('I want to display the page in {string}', (language) => {
-    const languageLink = cy.contains(language);
-    languageLink.click();
-});
--- cypress/e2e/common/page-navigation.js
+++
@@ -1,5 +0,0 @@
-import { And } from "cypress-cucumber-preprocessor/steps";
-
-And('The user navigates to the {string} page', (pageName) => {
-  cy.fixture('urls.json').then(urls => cy.visit(urls[pageName]));
-});
